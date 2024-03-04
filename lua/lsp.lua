2a9cf6991bd50940a6cd9bc324ac11c9d8aec6a8
wwwsensor <l4l@tuta.io>
2024-03-04 21:04:10 +0100

    Clean everything and improve lsp and cmp setup

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,49 +1,36 @@
---- LSP-ZERO -----------------
--- https://lsp-zero.netlify.app
--- Enable default LSP mappings at attachment
-local lsp_zero = require "lsp-zero"
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-------------------------------
-
--- LS ------------------------
--- Install
+--- LSP ----------------------
+-- Management
+-- LSP-ZERO : https://lsp-zero.netlify.app
+local lspz = require "lsp-zero"
 require "mason".setup {}
 require "mason-lspconfig".setup {
-  handlers = { lsp_zero.default_setup },
-  ensure_installed = { "lua_ls", "bashls" }
+  ensure_installed = { "lua_ls", "bashls" },
+  handlers = { lspz.default_setup }
 }
 -- Init & setup
-local ls = require "lspconfig"
-ls.lua_ls.setup {}
-ls.bashls.setup { filetypes = { "sh", "zsh" } }
+local lspc = require "lspconfig"
+lspc.lua_ls.setup {}
+lspc.bashls.setup { filetypes = { "sh", "zsh" } }
 ------------------------------
 
--- CMP -----------------------
+--- COMPLETION ---------------
 local cmp = require "cmp"
-local cmp_action = require "lsp-zero".cmp_action()
+local cmp_action = lspz.cmp_action()
 cmp.setup {
-  -- Format
+  completion = { completeopt = "menu,menuone,noinsert" },
   formatting = { fields = { "menu", "abbr", "kind" } },
-  -- Sources
+  preselect = "item",
   sources = {
-    { name = "nvim_lsp" }, { name = "luasnip" },
-    { name = "buffer" }, { name = "path" }
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" }
   },
-  -- Mappings
   mapping = {
-    ["<Tab>"] = cmp_action.tab_complete(),
-    ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
     ["<C-h>"] = cmp.mapping.abort(),
-    ["<C-j>"] = cmp.mapping(function()
-      if cmp.visible() then cmp.select_next_item({behavior = "insert"})
-      else cmp.complete() end
-    end),
-    ["<C-k>"] = cmp.mapping(function()
-      if cmp.visible() then cmp.select_prev_item({behavior = "insert"})
-      else cmp.complete() end
-    end),
+    ["<C-j>"] = cmp_action.luasnip_supertab(),
+    ["<C-k>"] = cmp_action.luasnip_shift_supertab(),
     ["<C-l>"] = cmp.mapping.confirm({select = false}),
   },
   -- Snippets
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
 require "nvim-treesitter.configs".setup {
   -- Parsers (can use `all`) (1-5 mandatory)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "bash", "go", "toml", "json", "yaml" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "bash", "toml", "json", "yaml" },
   auto_install = false,
   highlight = {
     enable = true,
     -- Disable treesitter if file is too big
     disable = function(lang, buf)
-      local max_filesize = 500000 * 1024 -- 500000 KB / 500 MB
+      local max_filesize = 200000 * 1024 -- 200000 KB / 200 MB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then return true end
     end
@@ -14,7 +14,7 @@ require "nvim-treesitter.configs".setup {
 }
 
 require "highlight-undo".setup {} require "typst-preview".setup {} require "leap".create_default_mappings {}
-require "better_escape".setup { mapping = { "jk", "jK", "Jk", "JK", "kj", "kJ", "Kj", "KJ" } }
 require "gitsigns".setup { preview_config = { border =  "rounded", row = 1, col = 1, } }
+require "better_escape".setup { mapping = { "jk", "jK", "Jk", "JK" } }
 require "spider".setup { skipInsignificantPunctuation = false }
 require "which-key".setup {} require "colorizer".setup ()
